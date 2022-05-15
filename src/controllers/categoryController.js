4a3c0e7006b8df766bc688b04993202bf7d2ad4f
alminaildirar <alminaildirar@gmail.com>
2022-05-16 02:54:40 +0300

    added get root categories controller added

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -2,8 +2,29 @@ const {
     getCategoryById,
     getCategoriesByParentId,
 } = require('../services/CategoryService');
+const { checkImageExist } = require('../helpers/checkImageExist');
+const BadRequestError = require('../exceptions/BadRequestError');
 
-const getRootCategories = async (req, res, next) => {};
+//-----------Get Root Categories-------------
+const getRootCategories = async (req, res, next) => {
+    const mainCategoryId = req.params.category;
+
+    try {
+        const roots = await getCategoriesByParentId('root');
+        const rootData = await getCategoryById(mainCategoryId);
+        let parentData = await getCategoriesByParentId(mainCategoryId);
+        if (!parentData || !rootData)
+            throw new BadRequestError('Category Not Found.');
+
+        checkImageExist(parentData);
+
+        return res
+            .status(200)
+            .render('rootCategory', { roots, rootData, parentData });
+    } catch (error) {
+        next(error);
+    }
+};
 
 const getParentCategories = async (req, res, next) => {};
 
