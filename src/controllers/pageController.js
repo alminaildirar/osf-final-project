c4fe8952b93b39b37e6810dc388e1bdfe987d7c5
alminaildirar <alminaildirar@gmail.com>
2022-05-16 03:01:16 +0300

    get index page controller modified

--- src/controllers/pageController.js
+++ src/controllers/pageController.js
@@ -1,4 +1,12 @@
+const { getCategoriesByParentId } = require('../services/CategoryService');
+
 const getIndexPage = async (req, res) => {
+    try {
+        const roots = await getCategoriesByParentId('root');
+        res.status(200).render('index', { roots });
+    } catch (error) {
+        next(error);
+    }
 };
 
 module.exports = { getIndexPage };
