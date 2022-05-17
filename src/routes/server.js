0ee58b9c6c260cdc5b1a6766e74fb492c61534de
alminaildirar <alminaildirar@gmail.com>
2022-05-17 19:15:14 +0300

    auth route added, the middleware which checks session , some dependencies installed

--- src/routes/server.js
+++ src/routes/server.js
@@ -1,5 +1,6 @@
 module.exports = (app) => {
     app.use('/', require('./pageRoute'));
     app.use('/categories', require('./categoryRoute'));
-    app.use("/products", require("./productRoute"));
+    app.use('/products', require('./productRoute'));
+    app.use('/auth', require('./authRoute'));
 };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const config = require('./config');
+const cookieParser = require("cookie-parser");
 const { errorHandler } = require('./middleware/errorHandler')
 
 dotenv.config();
@@ -12,6 +13,16 @@ app.set('view engine', 'ejs');
 //----------Middleware-----------
 app.use(express.json());
 app.use(express.static('public'));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+
+//--Global Variable--
+global.userIN = null;
+
+app.use('*', (req, res, next) => {
+    userIN = req.cookies.user;
+    next();
+  });
 
 //---Prevent to receiving GET request to /favicon.ico
 app.get('/favicon.ico', (req, res) => res.status(204));
