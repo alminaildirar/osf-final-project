6536481761bcae64825dc80946da5210cd987458
alminaildirar <alminaildirar@gmail.com>
2022-05-30 00:31:39 +0300

    added coverage

--- __tests__/cart.test.js
+++ __tests__/cart.test.js
@@ -7,7 +7,6 @@ const {
     getCartRequest,
     getCartProducts,
     addItemToCartRequest,
-    getCartProductsVariantIds,
     removeItemFromCartRequest,
     changeItemQuantityCartRequest,
     cartProductsLength,
